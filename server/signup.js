089eff597411ef7ddfe97bd0f4d34ab7c1714903
Kary <karina@arteko.mx>
2018-02-28 18:14:31 -0600

    SE ENCRIPTA CONTRASENA

--- server/signup.js
+++ server/signup.js
@@ -1,17 +1,26 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+
 const signup = express.Router()
+const saltRounds = 10
 
 signup.get('/', (req, res) => {
 	res.send('Signup')
 })
 
-signup.post('/signup', (req, res) => {
-	const name = 'Karina'
-	const lastname = 'Montenegro'
-	const email = 'karii.mont@gmail.com'
-	
-	res.send('Request Reset')
-})
+signup.post('/addUser', (req, res) => {
+	let password = '1234.'
+	const salt = bcrypt.genSaltSync(saltRounds)
+	password = bcrypt.hashSync(password, salt)
 
+	const userObj = {
+		name: 'Karina',
+		lastname: 'Montenegro',
+		email: 'karii.mont@gmail.com',
+		password
+	}
+
+	res.send(userObj)
+})
 
 module.exports = signup
