7020d40947f51a31aed6770dbca0b33240a2244b
Kary <karina@arteko.mx>
2018-02-28 18:15:03 -0600

    POST Y GET PARA CADA ACCION

--- server/login.js
+++ server/login.js
@@ -5,4 +5,9 @@ login.get('/', (req, res) => {
 	res.send('Login')
 })
 
+login.post('/validate', (req, res) => {
+	res.send('success').status(200)
+	console.log()
+})
+
 module.exports = login
--- server/reset.js
+++ server/reset.js
@@ -5,8 +5,18 @@ reset.get('/', (req, res) => {
 	res.send('Reset Password')
 })
 
-reset.get('/request', (req, res) => {
-	res.send('Request Reset')
+reset.post('/sendMail', (req, res) => {
+	// validate mail. sendmail with token
+})
+
+reset.get('/validate', (req, res) => {
+	// validate token and username
+})
+
+reset.post('/reset', (req, res) => {
+	// validate password,
+	// store password and changetoken
+	// reedirect to index
 })
 
 module.exports = reset
