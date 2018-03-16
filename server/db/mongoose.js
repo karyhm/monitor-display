c3e44a2f34c99d53fccba81f9a85cf0205b532d3
Kary <karina@arteko.mx>
2018-03-16 11:31:52 -0600

    BASE DE DATOS A MLAB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 
 // which promise library we wanna use
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/monitor-data')
-
+mongoose.connect('mongodb://Kary:1234.@ds155268.mlab.com:55268/monitor-data')
 module.exports = {
 	mongoose
 }
